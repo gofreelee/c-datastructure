e9ad0b3a828f3a2a244eb06ac1cdac72097c9d0b
handsomelee <1979432070@qq.com>
2019-10-26 22:47:20 +0800

    修改

--- symboltable/test.cpp
+++ symboltable/test.cpp
@@ -4,7 +4,11 @@ using namespace std;
 int main(){
     symbol_table<int,int> test;
     test.put(1,13);
-    cout<<test.get(1);   
-    cout<<test.size(); 
+    test.put(3,33);
+    test.put(4,23);
+    symbol_table<int,int> adc=test;
+    cout<<adc.get(3);   
+    cout<<adc.size(); 
+    cout<<adc.contain(4);
  
 }
