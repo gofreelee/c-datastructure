01c60e70e60794842072f04f9b1ca0202f434416
handsomelee <1979432070@qq.com>
2019-10-26 22:45:06 +0800

    自己实现的move

---
+++ hashmap/my_move.hpp
@@ -0,0 +1,24 @@
+#ifndef _MY_MOVE_H
+#define _MY_MOVE_H
+//先声明一个结构体用来把值　左值引用　右值引用　都转化成值
+template <typename T>
+struct remove_reference {
+    typedef T type;
+};
+
+template <typename T>
+struct remove_reference <T&> {
+    typedef T type;
+};
+
+template <typename T>
+struct remove_reference <T&&>  {
+    typedef T type;
+};
+
+template <typename T>
+inline typename remove_reference<T>::type && move(T&& value){
+    return static_cast<typename remove_reference<T>::type&&> (value);
+}
+
+#endif
